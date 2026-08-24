b9865fe9cfc8d89f2fd1ec101e4d577ccc9e65eb
Coolguy <mobilemc022@gmail.com>
2026-08-24 11:11:53 +0700

    Implement initial mod structure for KrispySkin

---
+++ src/main/java/com/krispyskin/mod/KrispySkin.java
@@ -0,0 +1,13 @@
+package com.krispyskin.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KrispySkin implements ModInitializer {
+
+    public static final String MOD_ID = "krispyskin";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[KrispySkin] Plugin integration initialized.");
+    }
+}
